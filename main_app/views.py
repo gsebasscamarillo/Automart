1114806736b410fdd8066a7aebfaa7c99c492b2c
gsebasscamarillo <54732141+gsebasscamarillo@users.noreply.github.com>
2019-09-21 11:43:32 -0500

    Agregamos el template para el index

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse #primer paso
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Bienvenido a AutoMart</h1>")
+    return render(request, 'index.html')
+    #return HttpResponse("<h1>Bienvenido a AutoMart</h1>") #primer paso
